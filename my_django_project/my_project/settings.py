8f2b1828d374bf0f24f43e7b23f98dfcea854de1
ZigEliaZag <eliakaliza50@icloud.com>
2025-11-01 17:08:42 +0100

    connected heroku to this project

--- my_django_project/my_project/settings.py
+++ my_django_project/my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bsh22e%+3zgbs+w7-$%e_&o#2ilgv*rs-ayejjo7&sg)cl@$mx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
